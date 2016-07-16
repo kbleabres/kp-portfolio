28c1c5a804823cfcfc4cb0ac00d32df9bb10ee7e
Kim Leabres <kbleabres@gmail.com>
2016-07-17 01:10:50 +0800

    Use turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,35 @@
 //= require react_ujs
 //= require components
 //= require_tree .
+//
+var App = {
+    onLoadFns: [],
+
+    onPageLoad: function(callback) {
+        this.onLoadFns.push(callback);
+    },
+
+    load: function() {
+        this.onLoadFns.forEach(function(callback) {
+            callback.call(this);
+        }, this);
+    }
+};
+
+$(function() {
+
+    Turbolinks.enableProgressBar();
+
+    // Called everytime turbolinks loads a new page
+    $(document).on("page:load", function() {
+        App.load();
+    });
+
+    // Called on initial full page load
+    //
+    // defer is used to allow all features to register
+    // their page load callbacks before App.load runs
+    setTimeout(function() {
+        App.load();
+    }, 0);
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(title = '')
+    title.present? ? title : 'Kp Portfolio'
+  end
+
+  def page_description(description)
+    description ? description : 'Online Portfolio'
+  end
 end
