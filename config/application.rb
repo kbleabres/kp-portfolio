1125662eb75acb263a1b5197b714bfbaf2baaf2f
Kim Leabres <kbleabres@gmail.com>
2016-07-17 11:58:35 +0800

    Configured for deployment to Heroku

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
-# require "active_record/railtie"
+require "active_record/railtie"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
@@ -20,6 +20,8 @@ Bundler.require(*Rails.groups)
 module KpPortfolio
   class Application < Rails::Application
     config.generators.template_engine :haml
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile=false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
